c47860d9bf667a6ed98006d881a092de30668c25
Kane Koo <kanekoo@gmail.com>
2019-02-23 19:56:59 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -35,9 +35,9 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   count = 0 
   while (count < number_of_times)
-  puts phrase
-  count +=1 
-end
+    puts phrase
+    count +=1 
+  end
 end
 
 def until_iterator(number_of_times)
